c36da319780088186f8f3073cf51c70fa0d9248d
WrathOfZombies <bhargav.krishna@outreach.io>
2021-05-18 22:31:11 -0700

    (feat) Adding two sum solution and validation

---
+++ problems/1-two-sum/solution.go
@@ -0,0 +1,22 @@
+package problems
+
+func main(nums []int, target int) []int {
+	numberMap := make(map[int]int)
+
+	// For each number, see if its compliment exists
+	for index, number := range nums {
+		compliment := target - number
+
+		// (Second value in a map assignment is a boolean)
+		// Find the compliment in the map and if so, return the result
+		if complimentIndex, found := numberMap[compliment]; found {
+			return []int{index, complimentIndex}
+		} else {
+			// Add the current number to the map to be found later
+			numberMap[number] = index
+		}
+	}
+
+	// Return default
+	return []int{0, 0}
+}
